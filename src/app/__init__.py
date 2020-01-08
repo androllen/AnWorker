39ce74ed5895ec211674ae62e2189b7f22bb22be
liuchunhui <androllen@hotmail.com>
2020-01-08 20:37:18 +0800

    update

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -2,16 +2,11 @@ from flask import Flask
 # from src import log as _log
 from .exts import log
 from .config import Config
-from flask_bootstrap import Bootstrap
 
-bootstrap = Bootstrap()
 
 def Main():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(Config)
-
-    bootstrap.init_app(app)
- 
 
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
