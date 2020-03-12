a0afe2aa8301cac31f7fd210af0539ac2623b91a
liuchunhui <androllen@hotmail.com>
2020-03-12 17:28:39 +0800

    update

--- src/app/mysql.py
+++ src/app/mysql.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# 
+#
 # Time: 2020/03/10 12:35:13
 # Contact: androllen#hotmail.com
 
 from app import db
 
+class Bing(db.Model):
+  # 设置表名
+    __tablename__ = 'bing'
+  # 设置为主键之后，自动自增长
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(20), unique=True, nullable=False)
+    title = db.Column(db.Text, nullable=True)
+    pub_date = db.Column(db.String(), nullable=True)
 
-def Bing(db):
-  pass
+    def __repr__(self):
+        return self.name
