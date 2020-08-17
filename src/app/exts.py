9b6fbc630f1c08a45980f8517c07d4323beb2b91
liuchunhui <androllen@hotmail.com>
2020-08-17 11:07:34 +0800

    update log

--- src/app/exts.py
+++ src/app/exts.py
@@ -4,11 +4,9 @@
 # Time: 2019/12/30 10:53:58
 # Contact: androllen#hotmail.com
 
-import  os
 from .log import Logger
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-_filename = os.path.join(os.getcwd(), 'logs', 'all.log')
 log = Logger(level='debug', filename=_filename)
